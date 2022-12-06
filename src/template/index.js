d1483e9354c05630776fefa2fa27d6421da5636a
Tilen Pogacnik <tilenpogacnik94@gmail.com>
2022-12-06 13:16:39 +0100

    Update Prettier options

--- src/template/index.js
+++ src/template/index.js
@@ -1,25 +1,25 @@
-import run from "aocrunner";
-import {part1, part2} from "./solution.js"
+import run from 'aocrunner';
+import { part1, part2 } from './solution.js';
 
 run({
-  part1: {
-    tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
-    ],
-    solution: part1,
-  },
-  part2: {
-    tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
-    ],
-    solution: part2,
-  },
-  trimTestInputs: true,
-  onlyTests: false,
+    part1: {
+        tests: [
+            // {
+            //   input: ``,
+            //   expected: "",
+            // },
+        ],
+        solution: part1,
+    },
+    part2: {
+        tests: [
+            // {
+            //   input: ``,
+            //   expected: "",
+            // },
+        ],
+        solution: part2,
+    },
+    trimTestInputs: true,
+    onlyTests: false,
 });
--- src/template/solution.js
+++ src/template/solution.js
@@ -2,43 +2,14 @@
 //Day XX: Puzzle Name
 
 const part1 = (rawInput) => {
-  const input = parseInput(rawInput);
+    const input = parseInput(rawInput);
 
-  return 0;
+    return 0;
 };
 
-const parseInput = (rawInput) => rawInput.split("\n");
+const parseInput = (rawInput) => rawInput.split('\n');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const part2 = (rawInput) => {return 0;};
-export {part1, part2};
+const part2 = (rawInput) => {
+    return 0;
+};
+export { part1, part2 };
